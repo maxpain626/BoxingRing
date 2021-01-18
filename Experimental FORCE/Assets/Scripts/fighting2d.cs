4201f211688c917c0d57852d1d7c3ec8149d137e
maxpain626 <smile1616@yandex.ru>
2021-01-19 01:01:02 +0300

    BoxingRing
    My first game

---
+++ Experimental FORCE/Assets/Scripts/fighting2d.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighting2d : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    public GameObject player;
+  
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+  
+    void Update()
+    {
+        float moveX = 0;
+        if ((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.D)))
+        moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * speed * moveX * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //strike();
+            player.GetComponent<Animator>().SetTrigger("Attack");
+        }
+    }
+}
